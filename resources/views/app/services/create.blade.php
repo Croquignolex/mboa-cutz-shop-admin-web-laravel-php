09e3ed4f6cf15647444f1981598bf9f1263e2814
Alex Stéphane NGOMBOL <gpetitalex10@gmail.com>
2020-10-07 05:19:35 +0100

    Improve service edit

--- resources/views/app/services/create.blade.php
+++ resources/views/app/services/create.blade.php
@@ -18,13 +18,77 @@
                     <div class="mx-5">@include('partials.error-message')</div>
                     <form action="{{ route('services.store') }}" method="POST">
                         @csrf
+                        <div class="row mb-2">
+                            <div class="col">
+                                @include('partials.form.toggle', [
+                                    'name' => 'En vedette',
+                                    'id' => 'featured',
+                                    'color' => 'info',
+                                    'value' => old('featured')
+                                ])
+                                @include('partials.form.toggle', [
+                                    'name' => 'Mailleur réservation',
+                                    'id' => 'most_asked',
+                                    'color' => 'primary',
+                                    'value' => old('most_asked')
+                                ])
+                            </div>
+                        </div>
                         <div class="row">
                             <div class="col-sm-6">
                                 @include('partials.form.input', [
-                                    'name' => 'Nom*',
-                                    'id' => 'name',
+                                    'name' => 'Nom (français)*',
+                                    'id' => 'fr_name',
                                     'type' => 'text',
-                                    'value' => old('name')
+                                    'value' => old('fr_name')
+                                ])
+                            </div>
+                            <div class="col-sm-6">
+                                @include('partials.form.input', [
+                                    'name' => 'Nom (anglais)*',
+                                    'id' => 'en_name',
+                                    'type' => 'text',
+                                    'value' => old('en_name')
+                                ])
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                @include('partials.form.input', [
+                                    'name' => 'Prix*',
+                                    'id' => 'price',
+                                    'type' => 'number',
+                                    'value' => old('price') ?? 0
+                                ])
+                            </div>
+                            <div class="col-sm-6">
+                                @include('partials.form.input', [
+                                    'name' => 'Reduction (%)*',
+                                    'id' => 'discount',
+                                    'type' => 'number',
+                                    'value' => old('discount') ?? 0
+                                ])
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                @include('partials.form.select', [
+                                    'name' => 'Catégorie*',
+                                    'id' => 'category',
+                                    'title' => 'Choisir une catégorie',
+                                    'value' => old('category'),
+                                    'options' => $categories,
+                                    'multi' => false
+                                ])
+                            </div>
+                            <div class="col-sm-6">
+                                @include('partials.form.select', [
+                                    'name' => 'Etiquettes',
+                                    'id' => 'tags',
+                                    'title' => 'Choisir des étiquettes',
+                                    'value' => old('tags') ?? [],
+                                    'options' => $tags,
+                                    'multi' => true
                                 ])
                             </div>
                         </div>
@@ -51,3 +115,5 @@
         </div>
     </div>
 @endsection
+
+@include('partials.select-scripts')
--- resources/views/app/services/edit.blade.php
+++ resources/views/app/services/edit.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
-@section('app.master.title', page_title('Modifier témoignage'))
+@section('app.master.title', page_title('Modifier service'))
 
 @section('app.breadcrumb')
     @include('partials.breadcrumb', [
-        'title' => 'Modifier témoignage',
+        'title' => 'Modifier service',
         'icon' => 'mdi mdi-shopping',
-        'chain' => ['Témoignages', 'Modifier témoignage']
+        'chain' => ['Services', 'Modifier service']
     ])
 @endsection
 
@@ -16,16 +16,86 @@
             <div class="card card-default">
                 <div class="card-body">
                     <div class="mx-5">@include('partials.error-message')</div>
-                    <form action="{{ route('testimonials.update', compact('testimonial')) }}" method="POST">
+                    <form action="{{ route('services.update', compact('service')) }}" method="POST">
                         @csrf
                         @method('PUT')
+                        <div class="row mb-2">
+                            <div class="col">
+                                @include('partials.form.toggle', [
+                                    'name' => 'En vedette',
+                                    'id' => 'featured',
+                                    'color' => 'info',
+                                    'value' => old('featured') ?? $service->is_featured
+                                ])
+                                @include('partials.form.toggle', [
+                                    'name' => 'Nouveau',
+                                    'id' => 'new',
+                                    'color' => 'success',
+                                    'value' => old('new') ?? $service->is_new
+                                ])
+                                @include('partials.form.toggle', [
+                                    'name' => 'Mailleur réservation',
+                                    'id' => 'most_asked',
+                                    'color' => 'primary',
+                                    'value' => old('most_asked') ?? $service->is_most_asked
+                                ])
+                            </div>
+                        </div>
                         <div class="row">
                             <div class="col-sm-6">
                                 @include('partials.form.input', [
-                                    'name' => 'Nom*',
-                                    'id' => 'name',
+                                    'name' => 'Nom (français)*',
+                                    'id' => 'fr_name',
                                     'type' => 'text',
-                                    'value' => old('name') ?? $testimonial->name
+                                    'value' => old('fr_name') ?? $service->fr_name
+                                ])
+                            </div>
+                            <div class="col-sm-6">
+                                @include('partials.form.input', [
+                                    'name' => 'Nom (anglais)*',
+                                    'id' => 'en_name',
+                                    'type' => 'text',
+                                    'value' => old('en_name') ?? $service->en_name
+                                ])
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                @include('partials.form.input', [
+                                    'name' => 'Prix*',
+                                    'id' => 'price',
+                                    'type' => 'number',
+                                    'value' => old('price') ?? $service->price
+                                ])
+                            </div>
+                            <div class="col-sm-6">
+                                @include('partials.form.input', [
+                                    'name' => 'Reduction (%)*',
+                                    'id' => 'discount',
+                                    'type' => 'number',
+                                    'value' => old('discount') ?? $service->discount
+                                ])
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                @include('partials.form.select', [
+                                    'name' => 'Catégorie*',
+                                    'id' => 'category',
+                                    'title' => 'Choisir une catégorie',
+                                    'value' => old('category') ?? $service->category->slug,
+                                    'options' => $categories,
+                                    'multi' => false
+                                ])
+                            </div>
+                            <div class="col-sm-6">
+                                @include('partials.form.select', [
+                                    'name' => 'Etiquettes',
+                                    'id' => 'tags',
+                                    'title' => 'Choisir des étiquettes',
+                                    'value' => old('tags') ?? $selectedTags,
+                                    'options' => $tags,
+                                    'multi' => true
                                 ])
                             </div>
                         </div>
@@ -34,14 +104,14 @@
                                 @include('partials.form.textarea', [
                                     'name' => 'Description (français)',
                                     'id' => 'fr_description',
-                                    'value' => old('fr_description') ?? $testimonial->fr_description
+                                    'value' => old('fr_description') ?? $service->fr_description
                                 ])
                             </div>
                             <div class="col-sm-6">
                                 @include('partials.form.textarea', [
                                     'name' => 'Description (anglais)',
                                     'id' => 'en_description',
-                                    'value' => old('en_description') ?? $testimonial->en_description
+                                    'value' => old('en_description') ?? $service->en_description
                                 ])
                             </div>
                         </div>
@@ -52,3 +122,5 @@
         </div>
     </div>
 @endsection
+
+@include('partials.select-scripts')
